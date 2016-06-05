bf2a47b4e5ec71e56e2fbe501ffd6287ae21bae4
Anton Malinskiy <anton@malinskiy.com>
2016-06-05 20:19:41 +0700

    Implement DuplicateProductError in constructor

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,33 @@
+require 'set'
+require_relative 'errors'
+
 class Product
-  @@products = []
+  @@products = Set.new
+
+  attr_reader :title
+  attr_accessor :price, :stock
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+
+    if @@products.add?(self).nil?
+      raise DuplicateProductError, "#{@title} already exists"
+    end
   end
 
   def self.all
     @@products
   end
+
+  def eql?(other)
+    self.title.eql? other.title
+  end
+
+  def hash
+    @title.hash
+  end
+
+
 end
